dc0144de530604f306c4a9b0a919322f5c6163ac
hlmifzi <hlmifzi@gmail.com>
2025-10-28 04:09:31 +0700

    feat/skeleton list candidate

---
+++ apps/web/src/app/admin/job-list/manage-candidate/[jobID]/loading.tsx
@@ -0,0 +1,26 @@
+import { SkeletonInput, SkeletonTable } from "@rakamin/ui";
+
+export default function Loading() {
+  return (
+    <div className="p-6">
+      {/* Header dengan skeleton input */}
+      <div className="mb-6">
+        <SkeletonInput className="w-12" />
+      </div>
+      
+      {/* Skeleton table dengan many rows */}
+      <SkeletonTable
+        columns={[
+          { width: "210px", isFixed: true },
+          { width: "189px" },
+          { width: "189px" },
+          { width: "189px" },
+          { width: "189px" },
+          { width: "125px" },
+          { width: "249px" },
+        ]}
+        rows={10}
+      />
+    </div>
+  );
+}
